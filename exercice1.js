3714bdd052fc33f90e8c0cccdbb8248dbbf4f171
lilia <lilianom59@outlook.fr>
2022-12-02 15:12:38 +0100

    list de courses

--- exercice1.js
+++ exercice1.js
@@ -12,6 +12,37 @@ let articles = [
 
 // Votre code à partir d'ici :
 
+let ul = document.getElementById('listeCourse')
+let addButton = document.getElementById('add')
+let delButton = document.getElementById('del')
+let input = document.getElementById('input')
+
+
+for (let obj of articles){
+    let list = document.createElement('li')
+    list.innerHTML = obj;
+    ul.appendChild(list)
+    /*
+    list.addEventListener("click", function (){
+        list.remove()
+    })
+    */
+}
+addButton.addEventListener("click", function (){
+    let newLi = document.createElement('li')
+    newLi.innerHTML = input.value;
+    ul.appendChild(newLi)
+    delButton.addEventListener("click", function (){
+        newLi.remove()
+    })
+})
+
+
+
+
+
+
+
 
 
 
